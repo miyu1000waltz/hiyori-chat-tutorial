4a4330284e62547d9a7c9aa4ee4347301365678e
rebun <gegegenorebun@gmail.com>
2026-09-15 12:45:57 +0900

    add

---
+++ Part2/AsrApiController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+// ダミーファイル。Part3にて公開し・使用予定。
+
+public class AsrApiController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
